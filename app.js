1dde2e4a6ff100b15fd677cd007469b865c9bdce
jacando.tos <sereicikas.t@gmail.com>
2020-05-29 15:11:50 +0200

    finishing up js

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const hourHand = document.querySelector(".hour");
 const minuteHand = document.querySelector(".minute");
 const secondHand = document.querySelector(".second");
 
+function rotateHand(element, angle) {
+  element.style.transform = `translateX(-50%) rotate(calc(${angle * 360} * 1deg))`
+}
+
 function runClock() {
   const currentDate = new Date();
   const secondsAngle = currentDate.getSeconds() / 60;
   const minutesAngle = (secondsAngle + currentDate.getMinutes()) / 60;
   const hoursAngle = (minutesAngle + currentDate.getHours()) / 12;
 
-  rotate(secondHand, secondsAngle);
-  rotate(minuteHand, minutesAngle);
-  rotate(hourHand, hoursAngle);
-}
-
-function rotate(element, angle) {
-  element.style.transform = `translateX(-50%) rotate(calc(${angle * 360} * 1deg))`
+  rotateHand(secondHand, secondsAngle);
+  rotateHand(minuteHand, minutesAngle);
+  rotateHand(hourHand, hoursAngle);
 }
 
 runClock();
